b5078629d70be9192e820f0cdea58964154f0c6b
Eugen <e.kusowenko@gmail.com>
2019-11-29 12:14:54 +0000

    Compiled Files added

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
 
 func main() {
 	fmt.Println("My name: AvePazuzu")
 	fmt.Println("My Friend's Name: Lamashtu")
+	reader := bufio.NewReader(os.Stdin)
+	reader.ReadString('\n')
 }

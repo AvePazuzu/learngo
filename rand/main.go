4d67cd464e4c56416eacc59c0c2b5078c6c2cae1
Eugen <e.kusowenko@gmail.com>
2020-01-31 18:53:20 +0100

    rand updated

--- rand/main.go
+++ rand/main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hallo, Gopher!")
+	guess := 10
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i != guess; {
+
+		i = rand.Intn(guess + 1)
+		fmt.Printf("%d ", i)
+	}
+	fmt.Println()
+	//for {
+	//	fmt.Printf("%v \n", time.Now().UnixNano())
+	//	time.Sleep(2500 * time.Millisecond)
+	//}
 }

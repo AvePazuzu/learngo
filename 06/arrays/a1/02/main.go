e6f92c4b4a0aa4df7ac432b4c54e2cb4a9646282
Eugen <e.kusowenko@gmail.com>
2020-02-06 10:16:44 +0100

    More solutions

--- 06/arrays/a1/02/main.go
+++ 06/arrays/a1/02/main.go
@@ -7,13 +7,21 @@ import (
 func main() {
 
 	var (
-		names     [3]string
-		distances [5]int
+		names     = [...]string{"Name1", "Name2", "Name3"}
+		distances = [5]int{123, 124, 125, 126, 127}
 		data      [5]byte
 		ratios    [1]float32
 		alives    [4]bool
 		zero      [0]byte
 	)
+
+	for i := 0; i < len(names); i++ {
+		fmt.Printf("%q \n", names[i])
+	}
+
+	for _, v := range distances {
+		fmt.Printf("%d \n", v)
+	}
 
 	// Print values & Types:
 	fmt.Printf("names:     %#v \n", names)
@@ -23,20 +31,20 @@ func main() {
 	fmt.Printf("alives:    %#v \n", alives)
 	fmt.Printf("zero:	   %#v \n", zero)
 
-	// Print only Types:
-	fmt.Printf("names:     %#T \n", names)
-	fmt.Printf("distances: %#T \n", distances)
-	fmt.Printf("data:      %#T \n", data)
-	fmt.Printf("ratios:    %#T \n", ratios)
-	fmt.Printf("alives:    %#T \n", alives)
-	fmt.Printf("zero:	   %#T \n", zero)
+	// // Print only Types:
+	// fmt.Printf("names:     %#T \n", names)
+	// fmt.Printf("distances: %#T \n", distances)
+	// fmt.Printf("data:      %#T \n", data)
+	// fmt.Printf("ratios:    %#T \n", ratios)
+	// fmt.Printf("alives:    %#T \n", alives)
+	// fmt.Printf("zero:	   %#T \n", zero)
 
-	// Print only the Values:
-	fmt.Printf("names:     %q \n", names)
-	fmt.Printf("distances: %v \n", distances)
-	fmt.Printf("data:      %v \n", data)
-	fmt.Printf("ratios:    %.2f \n", ratios)
-	fmt.Printf("alives:    %t \n", alives)
-	fmt.Printf("zero:	   %d \n", zero)
+	// // Print only the Values:
+	// fmt.Printf("names:     %q \n", names)
+	// fmt.Printf("distances: %v \n", distances)
+	// fmt.Printf("data:      %v \n", data)
+	// fmt.Printf("ratios:    %.2f \n", ratios)
+	// fmt.Printf("alives:    %t \n", alives)
+	// fmt.Printf("zero:	   %d \n", zero)
 
 }

eb5f58ecdda3c62217274b773f94ef9a351ad1d5
Eugen <e.kusowenko@gmail.com>
2020-02-07 17:40:29 +0100

    time elapsed added

--- 05/lables/03/main.go
+++ 05/lables/03/main.go
@@ -4,35 +4,43 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"time"
 )
 
-func main()  {
-	
+func main() {
+
+	t1 := time.Now()
+
 	args := os.Args
 	if len(args) < 2 {
 		fmt.Println("Give some Numbers!")
 		return
 	}
-	
-	toTest:
+
+toTest:
 	for _, v := range args[1:] {
 		n, err := strconv.Atoi(v)
 		if err != nil {
 			continue
-		}	
-		
+		}
+
 		num := make([]int, n)
 		for i, v := range num {
-			v = i+1
-			num[i] = v 
-		}	
-		
-		for _, v := range num[1:len(num)-1] {
-			if num[len(num)-1]%v == 0 {			
-				continue toTest		
-				}
-			}				
-			fmt.Printf("%d  ", num[len(num)-1])	
+			v = i + 1
+			num[i] = v
 		}
-		fmt.Println()	
+
+		for _, v := range num[1 : len(num)-1] {
+			if num[len(num)-1]%v == 0 {
+				continue toTest
+			}
+		}
+		fmt.Printf("%d  ", num[len(num)-1])
+	}
+	fmt.Println()
+	t2 := time.Now()
+	ellapsed := t2.Sub(t1)
+
+	fmt.Println(ellapsed)
+
 }

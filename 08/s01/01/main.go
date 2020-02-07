8e6c89c0497eb9c1b9026cd9c4597d1027034eb9
Eugen <e.kusowenko@gmail.com>
2020-02-07 18:09:25 +0100

    1st solution added

---
+++ 08/s01/01/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var (
+		names     []string
+		distances []float32
+		data      []byte
+		ratios    []float32
+		alives    []bool
+	)
+
+	fmt.Printf("names		:%#T %d %t \n", names, len(names), names == nil)
+	fmt.Printf("distances	:%#T %d %t \n", distances, len(distances), distances == nil)
+	fmt.Printf("data		:%#T %d %t \n", data, len(data), data == nil)
+	fmt.Printf("ratios		:%#T %d %t \n", ratios, len(ratios), ratios == nil)
+	fmt.Printf("alives		:%#T %d %t \n", alives, len(alives), alives == nil)
+
+}

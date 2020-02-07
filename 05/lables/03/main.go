2a547c20d196a18de7119ef2401e7fc51fa69f0f
Eugen <e.kusowenko@gmail.com>
2020-02-07 17:51:14 +0100

    comments added

--- 05/lables/03/main.go
+++ 05/lables/03/main.go
@@ -1,3 +1,7 @@
+/*  This program takes inputs from the console,
+ * converts them to int if possible and
+ * checks the inputs if they are primes. */
+
 package main
 
 import (
@@ -11,25 +15,30 @@ func main() {
 
 	t1 := time.Now()
 
+	// Conslose input and err handling
+	// returns a slice of strings
 	args := os.Args
 	if len(args) < 2 {
 		fmt.Println("Give some Numbers!")
 		return
 	}
-
+	// labled statement to return to
 toTest:
+	// each element is converted to int if possible
 	for _, v := range args[1:] {
 		n, err := strconv.Atoi(v)
 		if err != nil {
 			continue
 		}
-
+		// for every successfull to int conversion a
+		// an empty slice of len n is created and filled
+		// with the values asscending to n
 		num := make([]int, n)
 		for i, v := range num {
 			v = i + 1
 			num[i] = v
 		}
-
+		// each element of slice is modulo checked
 		for _, v := range num[1 : len(num)-1] {
 			if num[len(num)-1]%v == 0 {
 				continue toTest

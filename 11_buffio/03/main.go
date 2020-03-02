a78acb5691f38ecee72047a1dda4e99e724a61c6
Eugen <e.kusowenko@gmail.com>
2020-03-02 13:38:15 +0100

    3rd added

--- 11_buffio/03/main.go
+++ 11_buffio/03/main.go
@@ -4,16 +4,26 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strings"
 )
 
 func main() {
+
+	rx := regexp.MustCompile(`[^A-Za-z]+`)
+
 	in := bufio.NewScanner(os.Stdin)
+	in.Split(bufio.ScanWords)
 
-	//rx := regexp.MustCompile()
-
+	total, words := 0, make(map[string]int)
+	// here next token is each word. Scan() jups from word to word.
 	for in.Scan() {
-		fmt.Println(in.Text())
-
+		total++
+		word := rx.ReplaceAllString(in.Text(), "")
+		word = strings.ToLower(word)
+		// in value of the map is increased each time an already exsiting word appears
+		words[word]++
 	}
-
+	fmt.Printf("There are %d words, %d of them are unique.\n",
+		total, len(words))
 }

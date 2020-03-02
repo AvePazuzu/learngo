8261966f87f0f9d5a215ce096d1d6434f6f1e070
Eugen <e.kusowenko@gmail.com>
2020-03-02 10:19:10 +0100

    comments added

--- 11_buffio/02/main.go
+++ 11_buffio/02/main.go
@@ -11,8 +11,10 @@ func main() {
 	in.Split(bufio.ScanWords)
 
 	total, words := 0, make(map[string]int)
+	// here next token is each word. Scan() jups from word to word.
 	for in.Scan() {
 		total++
+		// in value of the map is increased each time an already exsiting word appears
 		words[in.Text()]++
 	}
 	fmt.Printf("There are %d words, %d of them are unique.\n",
